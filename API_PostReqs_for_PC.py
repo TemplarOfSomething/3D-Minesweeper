c23e78db325e48b7cf52214fa09dae8ec2133fb2
TemplarOfSomething <zachary9blackwell@outlook.com>
2023-05-03 12:28:21 -0500

    api template finished, needs to be implemented

--- API_PostReqs_for_PC.py
+++ API_PostReqs_for_PC.py
@@ -2,9 +2,13 @@ import requests
 
 POST_URL = "http:{ip}:{port}/LED_POST/"       #replace with ip and port from pi, since it hosts
 GET_URL = "http:{ip}:{port}/GPIO_GET/"
+PUT_URL = "http:{ip}:{post}/LED_PUT/"
 
 #post led
 requests.post(url=POST_URL, data={"value":value})
 
 #get gpio
 requests.get(url=GET_URL, params="{GPIO#}")
+
+#put (UPDATE) led
+requests.put(url=PUT_URL, data={"value":value})
--- API_server_for_pi.py
+++ API_server_for_pi.py
@@ -1,7 +1,5 @@
-from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 gpio_list = []
 led_value = {}
@@ -24,6 +22,11 @@ async def get_gpio(name: str):
     except ValueError:
         return "Value does not exist"
 
+@app.put("/GPIO_Put/")
+async def put_gpio(name:str, value:bool):
+    gpio_list[int(gpio_list.index(name)) + 1] = value
+    return gpio_list[int(gpio_list.index(name)) + 1]
+    
 #LED
 @app.post("/LED_Post/")
 async def post_led(value):
@@ -35,3 +38,10 @@ async def post_led(value):
 @app.get("/LED_Get/")
 async def get_led():
     return led_value["value"]
+
+@app.put("/LED_Put/")
+async def put_led(value):
+    led_value["value"] = value
+    return {
+        "value":value
+    }
