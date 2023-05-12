8980043ea121123a86fbf6d564fb05d60561d586
TemplarOfSomething <zachary9blackwell@outlook.com>
2023-05-12 10:32:08 -0500

    finale

--- GPIO_logic_for_pi.py
+++ GPIO_logic_for_pi.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 from API_PostReqs_for_pi import *
 from API_PostReqs_for_PC import POST_LED_URL
+from internetinfo import *
 
 #GPIO setup
 GPIO.setmode(GPIO.BCM)
@@ -26,9 +27,9 @@ GPIO.setup(BUTTON_ONE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BUTTON_TWO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #API setup
-TERMINATION_POST = "http://{ip}:{port}/Terminate_Post/"
-TERMINATION_GET = "http://{ip}:{port}/Terminate_Get/"
-TERMINATION_PUT = "http://{ip}:{port}/Terminate_Put/"
+TERMINATION_POST = "http://{}:{}/Terminate_Post/".format(IP, PORT)
+TERMINATION_GET = "http://{}:{}/Terminate_Get/".format(IP, PORT)
+TERMINATION_PUT = "http://{}:{}/Terminate_Put/".format(IP, PORT)
 
 requests.post(url=POST_URL, params={"name":"BUTTON_ONE", "value":False})
 requests.post(url=POST_URL, params={"name":"BUTTON_TWO", "value":False})
@@ -41,53 +42,61 @@ def put_req(name:str, value:bool):
 
 #main
 
+
 button_one_active = False
-button_two_active = False
 led_status = False
 terminate = False
 
 while True:
-    #button one for layer mode
+    #button one (layer mode)
+    """
     if GPIO.input(BUTTON_ONE) == GPIO.HIGH and button_one_active == True:
         put_req("BUTTON_ONE", False)
-        print("MODE OFF")
+        print("MODE_OFF")
         button_one_active = False
         sleep(1)
-    if GPIO.input(BUTTON_ONE) == GPIO.HIGH and button_one_active == False:
-        put_req("BUTTON_ONE", True)
-        print("MODE ON")
-        button_one_active = True
-        sleep(1)
-
-    #button two for minesweeper mode
-    if GPIO.input(BUTTON_TWO) == GPIO.HIGH and button_two_active == True:
-        put_req("BUTTON_TWO", False)
-        print("2 MODE OFF")
-        button_two_active = False
-        sleep(1)
-    if GPIO.input(BUTTON_TWO) == GPIO.HIGH and button_two_active == False:
+    """
+    if GPIO.input(BUTTON_TWO) == GPIO.HIGH:
         put_req("BUTTON_TWO", True)
-        print("2 MODE ON")
+        print("MODE ON")
         button_two_active = True
         sleep(1)
 
+    #button two (minesweeper mode)
+    if GPIO.input(BUTTON_ONE) == GPIO.HIGH and button_two_active == True:
+        put_req("BUTTON_ONE", False)
+        print("MODE_OFF")
+        button_one_active = False
+        sleep(1)
+    
+    if GPIO.input(BUTTON_ONE) == GPIO.HIGH and button_two_active == False:
+        put_req("BUTTON_ONE", True)
+        print("2 MODE ON")
+        button_one_active = True
+        sleep(1)
+
     #LED
-    if button_two_active == True:
+    if button_one_active == True:
         led_status = requests.get(url=GET_URL).json()
-        if led_status != "False" or False:                           #json conversion mishap?
-            GPIO.output(LED_LIST[led_status],GPIO.HIGH)
+        prev_led = led_status
+        if led_status != False:                           #json conversion mishap?
+            GPIO.output(LED_LIST[-int(led_status)],GPIO.HIGH)
         sleep(0.5)
     
-    if button_two_active == False:
-        if led_status != "False" or False:
-            GPIO.output(LED_LIST[led_status], GPIO.LOW)
+    if button_one_active == False:
+            GPIO.output(LED_LIST[-int(led_status)], GPIO.LOW)
     
     #Termination
-    if requests.get(url=TERMINATION_GET).json() == "True" or True:
+    if requests.get(url=TERMINATION_GET).json() == True:
         requests.put(url=POST_URL, params={"name":"BUTTON_ONE", "value":False})
         requests.put(url=POST_URL, params={"name":"BUTTON_TWO", "value":False})
         requests.put(url=POST_LED_URL, params={"value":False})
         requests.put(url=TERMINATION_POST, params={"value":False})
+        button_one_active = False
+        led_status = False
+        terminate = False
+    sleep(1)
+    #print(requests.get(url="http://{IP}:{}/GPIO_Get/", params={"name":"BUTTON_ONE"}).json()) #testing
 
         
     
